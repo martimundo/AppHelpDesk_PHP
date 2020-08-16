bdbe6963117f47cceb0cd9e32617131b6ec7fe40
Marcos <martimundo@gmail.com>
2020-08-16 18:36:00 -0300

    Aplicação criada na linguagem php versão 7

---
+++ app/valida_login.php
@@ -0,0 +1,48 @@
+<?php
+
+    session_start();  
+
+    
+    //Variavel para verificar se autenciação foi realizada com sucesso
+
+    $usuario_autenticado = false;
+    //array de usuário sem conexão com o BD
+    $usuarios = array(
+        array("email"=>"adm@teste.com.br", "senha"=>"123456"),
+        array("email"=>"user@teste.com.br", "senha"=>"abdc")
+    );
+
+   /* echo '<pre>';
+    print_r($usuarios);
+    echo'</pre>';*/
+
+    foreach($usuarios as $user){
+        
+        /*echo 'Usuario App: ' . $user['email'] . '/' . $user['senha'];
+        echo '<br />';
+        echo 'Usuario form:. ' . $_POST ['email'] . '/' . $_POST ['senha'];
+        echo '<br />';
+        */
+
+        if ($user['email'] == $_POST ['email'] && $user['senha'] == $_POST ['senha']){
+            $usuario_autenticado = true;
+        }
+
+        if($usuario_autenticado){
+            
+            echo "Usuário Autenticado com Sucesso!";
+            $_SESSION['autenticado'] = 'Sim';
+        }else{
+            
+            $_SESSION['autenticado'] = 'Não';
+            header("Location: index.php?login=erro");
+        }
+       
+
+        
+    }
+
+    
+
+
+?>
